58e35f7a3fc389fc80e81128abefc53b6f131913
Ojas-Poswal <ojasposwal@gmail.com>
2026-06-12 20:41:45 +0530

    day5

---
+++ Week2/Day5/Q2_StringCompression.java
@@ -0,0 +1,30 @@
+package Week2.Day5;
+
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+            char curr = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == curr) {
+                count++;
+                i++;
+            }
+
+            chars[index++] = curr;
+
+            if (count > 1) {
+                String num = String.valueOf(count);
+
+                for (char c : num.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
